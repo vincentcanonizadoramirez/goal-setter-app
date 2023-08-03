62a70eef8a0e60b338d29d286ff7b1a951aa02f6
Vincent Ramirez <vincentcanonizadoramirez@gmail.com>
2023-08-03 15:26:31 +0800

    Changed API_URL on auth and goal services

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/goals/';
+const API_URL = 'https://bored-erin-chipmunk.cyclic.cloud/api/goals/';
 
 // Create new goal
 const createGoal = async (goalData, token) => {
