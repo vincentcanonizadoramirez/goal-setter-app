a54a168d327114e7657e0ab24523af49641492db
Vincent Ramirez <vincentcanonizadoramirez@gmail.com>
2023-08-03 15:37:52 +0800

    Added CORS

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -10,6 +11,11 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
